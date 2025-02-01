d8efd9e1f362ab526223e3eb2754f88d4f45a8e6
Hussein Beshir <hussein.beshir100@gmail.com>
2025-02-01 11:23:02 +0300

    React first task completed!

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-amber-300 text-center font-black border-x-purple-500 text-5xl' >
+      <h1>App</h1></div>
+  )
+}
+
+export default App
