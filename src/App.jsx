4aa2e9e26f3b3b79d61687947946162d42f96ae6
Hussein Beshir <hussein.beshir100@gmail.com>
2025-02-02 18:25:58 +0300

    Card Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const App = () => {
+  let title = [1,2,3,4,5];
   return (
-    <div className='bg-amber-300 text-center font-black border-x-purple-500 text-5xl' >
-      <h1>App</h1></div>
+    <div className='bg-amber-600 w-100 h-40 mx-auto my-50 rounded-2xl text-center flex items-center font-black border-x-purple-500 ' >
+     <div className='w-50% h-50%'>
+      <img src="src\assets\anime-landscape-person-traveling - Copy.jpg" alt="" className=' rounded-2xl h-14 ml-2'/>
+     </div>
+     <div className='ml-2'>
+      <h1 className='text-black ml-0 text-left text-3xl'>Hussein Beshir</h1>
+      <p className='text-amber-200 mt-1  text-left'>You got new message!</p>
+     </div>
+          </div>
   )
 }
 
